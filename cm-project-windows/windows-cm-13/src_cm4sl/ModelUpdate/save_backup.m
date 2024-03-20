a014e8081612d0709ea8c742f37a502e9f898956
Yeonsoo Park <yeonsoo.park@ipg-automotive.com>
2024-03-20 16:27:32 +0900

    tested on k8s cluster

--- cm-project-windows/windows-cm-13/src_cm4sl/ModelUpdate/save_backup.m
+++
@@ -1,5 +0,0 @@
-% Save backup of model with <oldname><postfix_backup>
-if (exist([Mdl_BaseName, postfix_backup, Mdl_FileExt]) ~= 4)
-    [status,message,messageId] = copyfile (Mdl_FileName, [Mdl_BaseName, postfix_backup, Mdl_FileExt]);
-    fprintf('Backuped: %s -> %s\n', Mdl_FileName, [Mdl_BaseName, postfix_backup, Mdl_FileExt]);
-end
--- cm-project-windows/windows-cm-13/src_cm4sl/ModelUpdate/update_Annotations.m
+++
@@ -1,8 +0,0 @@
-% Updating the annotation text containing CarMaker version (valid for TM4SL models as well)
-annotationObjs = find_system(trg_mdl,'FindAll','on','Type','annotation');
-for j=1:numel(annotationObjs)
-    if regexp(get_param(annotationObjs(j),'PlainText'),OldVersion)
-        annObj = get_param(annotationObjs(j),'Object');
-        annObj.Text = NewVersion;
-    end
-end
